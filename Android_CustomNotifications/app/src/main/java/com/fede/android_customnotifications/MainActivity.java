1e4c1b04c23a6d112a9e5664c3cc5695a458e017
federico1525 <f.casali@gmail.com>
2016-05-20 13:30:01 -0700

    Android_CustomNotifications sample

--- Android_CustomNotifications/app/src/main/java/com/fede/android_customnotifications/MainActivity.java
+++ Android_CustomNotifications/app/src/main/java/com/fede/android_customnotifications/MainActivity.java
@@ -1,5 +1,6 @@
 package com.fede.android_customnotifications;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
