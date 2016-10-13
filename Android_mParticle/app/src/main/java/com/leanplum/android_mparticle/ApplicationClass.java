d3922689889c5440be5d2a573493d07f28810f84
federico1525 <f.casali@gmail.com>
2016-10-13 15:21:14 -0700

    Android_mParticle sample

--- Android_mParticle/app/src/main/java/com/leanplum/android_mparticle/ApplicationClass.java
+++ Android_mParticle/app/src/main/java/com/leanplum/android_mparticle/ApplicationClass.java
@@ -23,7 +23,6 @@ import com.mparticle.MParticle;
 
 public class ApplicationClass extends Application {
 
-
     @Override
     public void onCreate() {
         super.onCreate();
@@ -36,36 +35,18 @@ public class ApplicationClass extends Application {
 
         LeanplumPushService.setGcmSenderId(LeanplumPushService.LEANPLUM_SENDER_ID);
 
-        LeanplumPushService.setCustomizer(new LeanplumPushNotificationCustomizer() {
-            //
-            @Override
-            public void customize(NotificationCompat.Builder builder, Bundle notificationPayload) {
-
-                // Setting a custom smallIcon included in the Drawable folder
-                builder.setSmallIcon(R.drawable.atest);
-
-                // Setting a custom largeIcon included in the Drawable folder
-                Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.drawable.androidorange);
-                builder.setLargeIcon(largeIcon);
-
-                // Setting a custom Big Picture included in the Drawable folder, beneath the notification
-                Bitmap androidBanner = BitmapFactory.decodeResource(getResources(), R.drawable.androidappsdev);
-                builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(androidBanner));
-            }
-        });
-
 
         Leanplum.addStartResponseHandler(new StartCallback() {
             @Override
             public void onResponse(boolean b) {
-                Log.i("### ", "Leanplum started -- application class");
+                Log.i("### ", "Leanplum started!");
             }
         });
 
         // Starting MParticle - this will also start Leanplum
 //        MParticle.start(this, MParticle.InstallType.AutoDetect, MParticle.Environment.Production);
+//        MParticle.start(this, MParticle.InstallType.AutoDetect, MParticle.Environment.Development);
 
-//        MParticle.start(this, MParticle.InstallType.AutoDetect, MParticle.Environment.Production);
         MParticle.start(this);
 
     }
